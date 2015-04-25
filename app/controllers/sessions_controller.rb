6045dd2ab6c9915c50062f9ef5f3741e3f0f5843
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2015-04-25 20:19:40 +0900

    add session specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.where(email: params[:session][:email].downcase).first
+    if user.present? && user.authenticate(params[:session][:password])
+      login user
+      render nothing: true, status: 201
+    else
+      render nothing: true, status: 400
+    end
+  end
+
+  def destroy
+    logout
+    render nothing: true, status: 201
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe SessionController, type: :controller do
+RSpec.describe SessionsController, type: :controller do
+
+  describe "POST: #create" do
+    describe "if user exist find by email you inputed" do
+      before do
+        @user = User.create(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+      end
+      describe "then password is incorrect" do
+        it "you have not logined with 400" do
+          post :create, {session: {email: @user.email, password: "incorrect"}}
+          expect(response.status).to eq(400)
+          expect(login?).to be_falsy
+        end
+      end
+      describe "then password is correct" do
+        it "you have logined with 201" do
+          post :create, {session: {email: @user.email, password: @user.password}}
+          expect(response.status).to eq(201)
+          expect(login?).to be_truthy
+        end
+      end
+    end
+    describe "if user does not exist you inputed" do
+      it "you have not logined with 400" do
+        post :create, {session: {email: "noexist@example.com", password: "incorrect"}}
+        expect(response.status).to eq(400)
+        expect(login?).to be_falsy
+      end
+    end
+  end
 
 end
