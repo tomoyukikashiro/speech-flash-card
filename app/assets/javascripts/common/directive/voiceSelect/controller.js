6c01dcddf43aa3b61515e3fd4d4e8a812063957d
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2015-10-18 16:33:43 +0900

    add sample voice button

--- app/assets/javascripts/common/directive/voiceSelect/controller.js
+++ app/assets/javascripts/common/directive/voiceSelect/controller.js
@@ -9,16 +9,26 @@
 
   function voiceSelectController($scope, speech) {
     var vm = this;
+    var selectedVoice;
     vm.voices = speech.getVoices();
     vm.onSelect = onSelect;
+    vm.speakSample = speakSample;
+
+    function speakSample() {
+      if(!selectedVoice){
+        return;
+      }
+      speech.init({voice: selectedVoice});
+      speech.speak(speech.getSampleText(selectedVoice.lang));
+    }
 
     function onSelect($event) {
       if(!vm.selectedVoice){
         return;
       }
       var data = speech.getVoiceParam(vm.selectedVoice);
-      var voice = speech.getVoiceData(data.lang, data.name);
-      $scope.onSelect({voice: voice});
+      selectedVoice = speech.getVoiceData(data.lang, data.name);
+      $scope.onSelect({voice: selectedVoice});
     }
   }
 
